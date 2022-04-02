0c2c6fa56fe5101ca5e4e10f8fb7c362af0a7f3a
vikkn <vikaslagerweij@gmail.com>
2022-04-02 11:38:18 +0100

    del useless + Surround Priority enum

--- src/main/java/dev/zprestige/fire/module/combat/Surround.java
+++ src/main/java/dev/zprestige/fire/module/combat/Surround.java
@@ -231,4 +231,18 @@ public class Surround extends Module {
         }
         return -1;
     }
+
+    protected enum Priority{
+        HIGH(1000),
+        NORMAL(0);
+
+        private final int priority;
+        Priority(int priority){
+            this.priority = priority;
+        }
+
+        public int getPriority() {
+            return priority;
+        }
+    }
 }
